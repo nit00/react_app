085f434a980c5d17bc31393ab7ea927d694dfe8e
Nitin Patel <user_00@Nitins-MacBook-Air.local>
2022-10-24 11:39:19 +0530

    commit

---
+++ diwali2/src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+import Background from './Particle';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+
+const Home=()=>{
+    
+    return (
+    <>
+<Background/>
+<h1 id="happy">Happy</h1><br></br>
+<h1 id="diwali">Diwali</h1>
+    </>
+)
+}
+
+export default Home;
